5511ca3fb65a074684995e832d4266319831c53d
prabhu157 <prabhu.mani@eargo.com>
2019-03-14 10:49:30 -0700

    added few lines

--- sample_py.py
+++ sample_py.py
@@ -1,8 +1,13 @@
+import pytest
+import logging
 
+logger = logging.getLogger('')
 
 def main():
      print "-Printing the line: : : sample python file"
      print "End"
+     logger.info('just an info')
+     assert True
 
 
 if __name__== "__main__":
